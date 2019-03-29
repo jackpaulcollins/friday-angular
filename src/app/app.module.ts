64536f14412f676473f88960170c4ef35f84747c
Student <example@example.com>
2019-03-29 09:21:04 -0700

    build feed component and begin to develop it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageProfileTileComponent } from './home-page-profile-tile/home-page-profile-tile.component';
+import { ProfileFeedComponent } from './profile-feed/profile-feed.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomePageProfileTileComponent } from './home-page-profile-tile/home-page
     AppComponent,
     NavBarComponent,
     HomePageProfileTileComponent,
+    ProfileFeedComponent,
   ],
   imports: [
     BrowserModule
