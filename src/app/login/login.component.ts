7188fee2f193e7d223f1454e0bc17a01f738a633
Student <example@example.com>
2019-04-05 15:13:37 -0700

    add ability for user to create account and save name and password to database

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+import {User} from '../models/users.model';
+import {UserService} from '../user.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UserService) { }
   login(){
     this.router.navigate(['home']);
+  }
+
+  createAccount(username: string, password: string){
+    const newUser: User = new User(username,password);
+    this.userService.addUser(newUser);
   }
 
   ngOnInit() {
